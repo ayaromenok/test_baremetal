8d231b6b4c740e2ad8e22d61923688cc955ff114
Andrey Yaromenok <ayaromenok@gmail.com>
2021-12-14 07:50:37 +0200

    #2 Lattitude, Longitude, Height, date/time + Number of sattelites

--- 02__serial_GPS/serial_GPS/serial_GPS.ino
+++ 02__serial_GPS/serial_GPS/serial_GPS.ino
@@ -1,11 +1,35 @@
 #include <HardwareSerial.h>
-HardwareSerial nss(2);
-   
+#include <TinyGPS.h>
+
+//hot start around 2 min
+
+HardwareSerial tty(2); 
+TinyGPS gps;
+
 void setup(void) {
-  Serial.begin(115200);
-  nss.begin(9600);
+  Serial.begin(115200);  
+  tty.begin(9600, SERIAL_8N1, 16, 17);
   Serial.println("Fun begins:");
 }
+  
+
+    
 void loop(void) {
-  if (nss.available()) Serial.write(nss.read());
+float flat, flon;
+unsigned long age, _date, _time, _age;  
+  //if (tty.available()) Serial.write(tty.read());
+   // while (tty.available() >0) {
+   if (tty.available()){
+       gps.encode(tty.read());
+    }
+    
+    gps.f_get_position(&flat, &flon, &age);
+    gps.get_datetime(&_date, &_time, &_age);
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);Serial.print("N, ");  
+    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);Serial.print("E, "); 
+    Serial.print(gps.altitude()/100.0f);Serial.print(" meters"); // in cm originally 
+    Serial.print(",  Date:"); Serial.print(_date);
+    Serial.print(",  Time:"); Serial.print(_time);
+    Serial.print(",  N of sats:");  Serial.println(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    delay(1000);
 }
