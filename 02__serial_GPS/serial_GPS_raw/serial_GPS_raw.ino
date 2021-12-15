d56bf75de352adc076057ca2a312de555ec9d5d1
Andrey Yaromenok <ayaromenok@gmail.com>
2021-12-15 18:32:39 +0200

    #2 added serial raw data display to TTY

---
+++ 02__serial_GPS/serial_GPS_raw/serial_GPS_raw.ino
@@ -0,0 +1,18 @@
+#include <HardwareSerial.h>
+
+HardwareSerial tty(2); 
+
+void setup(void) {
+  Serial.begin(115200);  
+  tty.begin(9600, SERIAL_8N1, 16, 17);
+  Serial.println("Fun begins:");
+}
+  
+    
+void loop(void) {  
+   if (tty.available()){
+//       gps.encode(tty.read());
+       Serial.write(tty.read());
+    }
+    
+}
