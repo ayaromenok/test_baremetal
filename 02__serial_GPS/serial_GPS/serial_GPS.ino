5937417fc2a8d02c803ef95bf5dc662d7048ec08
Andrey Yaromenok <ayaromenok@gmail.com>
2022-01-10 11:53:25 +0200

    #2 added LED blinking for search mode

--- 02__serial_GPS/serial_GPS/serial_GPS.ino
+++ 02__serial_GPS/serial_GPS/serial_GPS.ino
@@ -10,24 +10,35 @@ void setup(void) {
   Serial.begin(115200);  
   tty.begin(9600, SERIAL_8N1, 16, 17);
   Serial.println("Fun begins:");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
   
 float flat, flon;
 unsigned long age, _date, _time, _age;  
+int lp_count=0;
     
 void loop(void) {  
    if (tty.available()){
-       gps.encode(tty.read());
-       Serial.write(tty.read());
-    }
-    
-    gps.f_get_position(&flat, &flon, &age);
-    gps.get_datetime(&_date, &_time, &_age);
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);Serial.print("N, ");  
-    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);Serial.print("E, "); 
-    Serial.print(gps.altitude()/100.0f);Serial.print(" meters"); // in cm originally 
-    Serial.print(",  Date:"); Serial.print(_date);
-    Serial.print(",  Time:"); Serial.print(_time);
-    Serial.print(",  N of sats:");  Serial.println(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+      gps.encode(tty.read());          
+      gps.f_get_position(&flat, &flon, &age);
+      gps.get_datetime(&_date, &_time, &_age);
+      if (flat != TinyGPS::GPS_INVALID_F_ANGLE){
+        Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);Serial.print("N, ");  
+        Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);Serial.print("E, "); 
+        Serial.print(gps.altitude()/100.0f);Serial.print(" meters"); // in cm originally 
+        Serial.print(",  Date:"); Serial.print(_date);
+        Serial.print(",  Time:"); Serial.print(_time);
+        Serial.print(",  N of sats:");  Serial.println(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+        digitalWrite(LED_BUILTIN, HIGH);
+      }else{
+        Serial.print("# sats:");Serial.println(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());        
+        if ((lp_count%2)!=0){
+          digitalWrite(LED_BUILTIN, HIGH);
+        }else{
+          digitalWrite(LED_BUILTIN, LOW);
+        }
+        lp_count++;
+      }//gps data
+    }//if tty
     delay(1000); 
 }
